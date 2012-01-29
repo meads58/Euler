9278f8a4895850a7291861204bde17662f57392d
meads58 <meads109@gmail.com>
2012-01-29 14:08:36 +0000

    Made a bit faster

--- ProjectEulerProblems/problem8.scm
+++ ProjectEulerProblems/problem8.scm
@@ -2,35 +2,26 @@
 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450 
 ) 
 
-
-(define (list-5 list-nums)
-        (if (< (count list-nums) 5) '()
-                (list   (item 1 list-nums)
-                        (item 2 list-nums)
-                        (item 3 list-nums)
-                        (item 4 list-nums)
-                        (item 5 list-nums))))           
- 
- 
-(define (sum-5 list-num)
-  (if (< (length list-num) 5) 1
-        (* (list-ref list-num 0)
-           (list-ref list-num 1)
-           (list-ref list-num 2)
-           (list-ref list-num 3)
-           (list-ref list-num 4))))
-          
+(define (item-5 x)
+  (list (item 1 x)
+        (item 2 x)
+        (item 3 x)
+        (item 4 x)
+        (item 5 x)))
 
 
-(define (find-5-total total next-total list-num)
-      (cond    ((< (count list-num) 5) total)
-               ((< total next-total) (find-5-total next-total (sum-5 (list-5 (butfirst list-num))) (butfirst list-num)))
-               (else (find-5-total total (sum-5 (list-5 (butfirst list-num))) (butfirst list-num)))))                                  
-               
-(define (main mainList)
-(find-5-total 1 (sum-5 (list-5 mainList)) mainList))
+(define (sum-5 total next-total BList)          
+  (cond ((< (count BList)5) total)
+        ((< total next-total )
+         (sum-5 next-total (apply * (item-5 BList)) (butfirst BList)))
+        (else (sum-5 total (apply * (item-5 BList)) (butfirst BList)))))
+
+(define (main bList)
+(sum-5 1 (apply * (item-5 bList)) bList))
+  
 
 (main bigList)
+
 
 
 
