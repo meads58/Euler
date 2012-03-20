79970095bff4e7e45a05caf552be7bc0ccfa96f2
meads58 <meads109@gmail.com>
2012-03-20 13:36:23 +0000

    Solved

--- ProjectEulerProblems/problem9.scm
+++ ProjectEulerProblems/problem9.scm
@@ -12,35 +12,39 @@
 ;a + b + c = 1000  
 
 
-(define (check-Order? a b c)
-  (if (and (< a b) (< b c)) #t
-      #f))
+(define (square x)
+  (* x x))
+
+(define (eulids m n) ;Creates a list of triples 
+   (list (calcA m n)
+         (calcB m n)
+         (calcC m n)))
+
+(define (calcA m n) ;Calculates side A
+  (* 2 m n))
+
+(define (calcB m n) ;Calculates side B
+  (-(square m)(square n)))
+
+(define (calcC m n) ;Calculates side C the hypotenuse
+  (+(square m) (square n)))
+
+(define (total m n d);Adds a b c togethers and multiples by the d parameter. Increase d will find more combinations of triples
+  (* d (apply + (eulids m n))))
+
+; Does the pythagorean triple add up to the max value?
+(define (eulids? m n d max)
+  (cond ((= d 100000) '(Can't find a triple matching that total))
+        ((> (total m n d) (* 2 max)) (eulids? 2 1 (+ d 1) max))
+        ((= (total m n d) max)
+          (* d (apply * (eulids m n))))          
+            ;and (= (total m n d) max) 
+            ;  (< (calcA m n) (calcB m n))
+            ;  (< (calcB m n) (calcC m n)))
+            ;(se(eulids m n) '(multiply each value by ) d))
+        ((< (+ n 1) m) (eulids? m (+ n 1) d max))
+        ((= (+ n 1) m) (eulids? (+ m 1) 1 d max))
+        (else #f)))
 
 
-(define (check1000? a b c)
-  (if (= (+ a b c) 1000) #t
-      #f)) 
-
-(define (check-triple? a b c)
-  (if (= (+ (* a a) (* b b)) (* c c)) #t
-      #f))
-
-          
-         
-(define (isPyth? a b c)
-  (cond ((and (check-Order? a b c)
-              (check1000? a b c)
-              (check-triple? a b c)) '(all of them))
-        ((check1000? a b c) 'order)
-        (else '(not))))
-        
-         
-(isPyth?  3 4 5)
-
-
-
-
-
-
-
-
+(eulids? 2 1 1 1000)
